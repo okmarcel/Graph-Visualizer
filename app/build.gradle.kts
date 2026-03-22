f4ba0e7d8f24e9106902c417ae4790f24dcdcd5b
okmarcel <marcelgladysz44@gmail.com>
2026-03-22 10:14:13 +0100

    Added JavaFX package and displayed a window

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+	id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 repositories {
@@ -30,6 +31,11 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
+}
+
+javafx {
+	version = "21"
+	modules = listOf("javafx.controls", "javafx.fxml")
 }
 
 application {
