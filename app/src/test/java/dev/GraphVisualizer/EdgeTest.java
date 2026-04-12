e76647f03219ee19cfcc56af37010a098fc914c8
Jakub Piela <173794188+RavenCS1@users.noreply.github.com>
2026-04-12 09:55:42 +0200

    EdgeTest GraphTest

---
+++ app/src/test/java/dev/GraphVisualizer/EdgeTest.java
@@ -0,0 +1,10 @@
+package dev.GraphVisualizer;
+
+import dev.GraphVisualizer.models.Edge;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EdgeTest {
+    
+}
---
+++ app/src/test/java/dev/GraphVisualizer/GraphTest.java
@@ -0,0 +1,10 @@
+package dev.GraphVisualizer;
+
+import dev.GraphVisualizer.models.Graph;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GraphTest {
+    
+}
